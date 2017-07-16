14e94e2b7cc279e085ef6cc15894ac389bd367bf
Radoslav Stankov <rstankov@gmail.com>
2017-07-17 00:57:49 +0300

    Move function back as internal

--- src/react-mini-form/withForm.js
+++ src/react-mini-form/withForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import equals from './equals';
 
 export default (getState) => (Component) => {
   return class WithForm extends React.Component {
@@ -43,3 +42,27 @@ export default (getState) => (Component) => {
     }
   };
 };
+
+export default function equals(object1, object2) {
+  for(let key in object1) {
+    if (object1[key] === object2[key]) {
+      continue;
+    }
+
+    if (!Array.isArray(object1[key]) || !Array.isArray(object2[key])) {
+      return false;
+    }
+
+    if (object1[key].length !== object2[key].length) {
+        return false;
+    }
+
+    for(let i = 0, l = object1[key].length; i < l; i++) {
+      if (object1[key][i] !== object2[key][i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
