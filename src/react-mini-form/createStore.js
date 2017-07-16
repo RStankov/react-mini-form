7adde3a4f62fb0683df4ac298490ce64b651e54b
Radoslav Stankov <rstankov@gmail.com>
2017-07-16 23:49:12 +0300

    Reduce number of unneeded re-renderers

--- src/react-mini-form/createStore.js
+++ src/react-mini-form/createStore.js
@@ -1,6 +1,7 @@
 import createValidator from './createValidator';
 import createEmitter from './createEmitter';
 import createDebouncer from './createDebouncer';
+import equals from './equals';
 
 export default function createStore({ values, validations }) {
   const { subscribe, emit } = createEmitter();
@@ -13,6 +14,21 @@ export default function createStore({ values, validations }) {
     errors: {},
     status: 'default',
   };
+
+  function validateField(name) {
+    const error = validate(name, state.values[name]);
+
+    if (equals(error, state.errors[name] || [])) {
+      return;
+    }
+
+    state.errors = {
+      ...state.errors,
+      [name]: validate(name, state.values[name]),
+    };
+
+    emit();
+  }
 
   return {
     subscribe,
@@ -43,17 +59,16 @@ export default function createStore({ values, validations }) {
     handleInputBlur(e) {
       const name = e.target.name;
 
-      state.errors = {
-        ...state.errors,
-        [name]: validate(name, state.values[name]),
-      };
-
-      emit();
+      validateField(name);
     },
 
     handleInputChange(e) {
       const name = e.target.name;
       const value = e.target.value;
+
+      if (value === state.values[name]) {
+        return;
+      }
 
       state.values = {
         ...state.values,
@@ -62,14 +77,7 @@ export default function createStore({ values, validations }) {
 
       emit();
 
-      debounce(name, (name) => {
-        state.errors = {
-          ...state.errors,
-          [name]: validate(name, state.values[name]),
-        };
-
-        emit();
-      });
+      debounce(name, validateField);
     },
   }
 }
--- src/react-mini-form/withForm.js
+++ src/react-mini-form/withForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import equals from './equals';
 
 export default (getState) => (Component) => {
   return class WithForm extends React.Component {
@@ -42,27 +43,3 @@ export default (getState) => (Component) => {
     }
   };
 };
-
-function equals(object1, object2) {
-  for(let key in object1) {
-    if (object1[key] === object2[key]) {
-      continue;
-    }
-
-    if (!Array.isArray(object1[key]) || !Array.isArray(object2[key])) {
-      return false;
-    }
-
-    if (object1[key].length !== object2[key].length) {
-        return false;
-    }
-
-    for(let i = 0, l = object1[key].length; i < l; i++) {
-      if (object1[key][i] !== object2[key][i]) {
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
